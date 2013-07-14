0194672084bb7ff609f7d4021383a331a549e3c9
brian <me@brianwhitton.com>
2013-07-14 00:30:41 -0400

    adding a go again button and loading indicator. text is verticallyu aligned

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
+var $loader = $('.loader')
+var container = document.getElementById('container')
 var A = {
     blinds : document.getElementsByClassName('blind'),
     index : 0,
-    setCurrent : function(){this.current = this.blinds[this.index]},
+    setCurrent : function(i){
+        if (arguments.length > 0) {this.index = arguments[i]}
+        this.current = this.blinds[i] || this.blinds[this.index]
+    },
     open : function(){
         this.current.classList.remove('closed')
         this.current.classList.add('opened')
@@ -23,4 +28,29 @@ function init(){
     setTimeout(A.stagger.bind(A), 750)
 }
 
+function again(){
+    $loader.addClass('begin') 
+    A.setCurrent(0)
+    $(A.blinds).each(function(i,e){
+        e.classList.remove('opened')
+        e.classList.add('closed')
+    })
+    A.again = true
+}
+
+container.addEventListener('webkitTransitionEnd', againHandler)
+container.addEventListener('transitionEnd', againHandler)
+container.addEventListener('transitionend', againHandler)
+
+function againHandler(e){
+    if (e.target === A.blinds[0] && A.again){
+        init()
+        A.again = false
+    } else if ( e.target === A.blinds[3]) {
+        $loader.removeClass('begin')
+    } else { return }
+}
+
 init()
+
+$('#again').click(again)
