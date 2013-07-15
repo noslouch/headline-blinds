4f7a970d59435d09ba88539b648dd58b3aa9ca9a
brian <me@brianwhitton.com>
2013-07-15 18:38:53 -0400

    cleaned up code

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,8 @@
 /*jshint -W002*/
 
 var quotes = document.getElementsByTagName('h1'),
-    slides = [],
-    $loader = $('.loader'),
     container = document.getElementById('container'),
+    slides = [],
     animation
 
 
@@ -109,63 +108,7 @@ $(container).on('webkitTransitionEnd', animationHandler)
 $(container).on('transitionEnd', animationHandler)
 $(container).on('transitionend', animationHandler)
 
-var A = {
-    blinds : document.getElementsByClassName('blind'),
-    index : 0,
-    setCurrent : function(i){
-        if (arguments.length > 0) {this.index = arguments[0]}
-        this.current = this.blinds[i] || this.blinds[this.index]
-    },
-    open : function(){
-        this.current.classList.remove('closed')
-        this.current.classList.add('opened')
-    },
-    close : function(){
-        this.current.classList.remove('opened')
-        this.current.classList.add('closed')
-    },
-    staggerOpen : function(){
-        this.setCurrent()
-        this.open()
-        this.index++
-        if ( this.index > this.blinds.length - 1 ){ return;}
-        setTimeout(this.staggerOpen.bind(A), 110)
-    },
-    staggerClose : function(){
-        this.setCurrent()
-        this.close()
-        this.index--
-        if ( this.index > this.blinds.length - 1 ) {return;}
-        setTimeout(this.staggerClose.bind(A), 110)
-    }
-}
 /*
-function init(){
-    setTimeout(A.staggerOpen.bind(A), 250)
-    container.addEventListener('webkitTransitionEnd', againHandler)
-    container.addEventListener('transitionEnd', againHandler)
-    container.addEventListener('transitionend', againHandler)
-}
-
-function again(){
-    $loader.addClass('begin') 
-    A.setCurrent(0)
-    $(A.blinds).each(function(i,e){
-        e.classList.remove('opened')
-        e.classList.add('closed')
-    })
-    A.again = true
-}
-
-function againHandler(e){
-    if (e.target === A.blinds[0] && A.again){
-        setTimeout(A.staggerOpen.bind(A), 100)
-        A.again = false
-    } else if ( e.target === A.blinds[A.blinds.length-1]) {
-        $loader.removeClass('begin')
-    } else { return }
-}
-*/
 function valueSwap($input1, $input2){
     $input1.change(function(e){
         $input2.val(e.target.value)
@@ -174,34 +117,14 @@ function valueSwap($input1, $input2){
         $input1.val(e.target.value)
     })
 }
+*/
 
-
-for (var i = 0; i < quotes.length; i++){
-    slides.push(new Slide(quotes[i]))
-}
 
 $(function(){
-    var $fadeSlider = $('#duration'),
-        $fadeText = $('#durationText'),
-        $fade = $($fadeSlider, $fadeText),
-        $staggerSlider = $('#stagger'),
-        $staggerText = $('#staggerText'),
-        $stagger = $($staggerText, $staggerSlider),
-        $controls = $('#controls')
 
-    $fadeText.val($fadeSlider.val())
-    $staggerText.val($staggerSlider.val())
-    
-    valueSwap($fadeSlider, $fadeText)
-    valueSwap($staggerSlider, $staggerText)
-    
-    $controls.on('change', $fade, function(e){ 
-        $(A.blinds).css({
-            webkitTransitionDuration : e.target.value + 'ms',
-            mozTransitionDuration : e.target.value + 'ms',
-            transitionDuration : e.target.value + 'ms'
-        })
-    })
+    for (var i = 0; i < quotes.length; i++){
+        slides.push(new Slide(quotes[i]))
+    }
 
     slides[0].staggerOpen()
 })
